95d51ba297956ba78ec2a22e10ca99c452ba08ad
tjh <2415430981@qq.com>
2025-12-30 09:31:15 +0800

    组件

--- src/core/baseparse.ts
+++ src/core/baseparse.ts
@@ -25,3 +25,49 @@ export class Baseparse<T=any> implements Parse<T>{
        }
    }
 }
+/*
+import {Pagination, ConfigProvider} from 'antd';
+import zhCN from 'antd/lib/locale/zh_CN';
+import {Flex} from 'antd';
+import styles from './index.module.less';
+
+export interface PaginationProps {
+  page: number;
+  pageSize: number;
+  total: number;
+  onChange?: (page: number, pageSize: number) => void
+}
+
+export const CommonPagination: React.FC<PaginationProps> = ({page, pageSize, total, onChange}) => {
+  return (
+    <div className={styles.pageContainer}>
+      <Flex justify={'space-between'} align={"center"}>
+        <div>共{total}条</div>
+        {total > 50 && <ConfigProvider locale={zhCN} theme={{
+          token: {
+            colorBorder: "#DDE0EE",
+            colorText: "rgba(0, 0, 0, 0.65)",
+          },
+        }}>
+            <Pagination
+                className={styles.paginationStyle}
+                total={total}
+                current={page}
+                pageSize={pageSize}
+                pageSizeOptions={[50, 100]}
+                showSizeChanger
+                showQuickJumper
+                onChange={(page, pageSize) => {
+                  onChange(page, pageSize)
+                }}
+            />
+        </ConfigProvider>}
+
+      </Flex>
+    </div>
+  );
+}
+
+
+
+*/
